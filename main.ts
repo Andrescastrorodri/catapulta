c03df0bf7089bf4c83b0fe50713a53eefdf32376
Andrescastrorodri <132184052+Andrescastrorodri@users.noreply.github.com>
2023-10-23 07:44:24 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,18 @@
+function Victoria () {
+    music.beginMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+}
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         // Si el botón A está presionado, hacer que los servos retrocedan rápido.
         pins.servoWritePin(AnalogPin.P0, 0)
         pins.servoWritePin(AnalogPin.P1, 180)
+        basic.showIcon(IconNames.Yes)
+        Victoria()
     } else {
         // Si el botón A no está presionado, hacer que los servos se muevan en sentido contrario durante 1 segundo.
         pins.servoWritePin(AnalogPin.P0, 180)
         pins.servoWritePin(AnalogPin.P1, 0)
-        basic.pause(1000)  // Espera 1 segundo
+        // Espera 1 segundo
+        basic.pause(1000)
     }
 })
-
-
